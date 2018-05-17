d1b45ba0687365c623ca943e8df278f0f5181229
CAMHub PublishBot <octravis@us.ibm.com>
2018-05-17 19:56:13 +0000

    Automated CAMHub::publish on 2018-05-17 19:56:13 UTC

--- chef/cookbooks/wmq/metadata.rb
+++ chef/cookbooks/wmq/metadata.rb
@@ -162,7 +162,7 @@ default['ibm_internal']['vault']['item']       - Vault Item to reference, will b
 ```
 EOH
 
-version '1.0.3'
+version '1.0.4'
 attribute 'wmq/advanced',
           :choice => ['true', 'false'],
           :default => 'false',
