b975a32d6257eb26d69406eb416b6a358f3b6da8
CAMHub PublishBot <octravis@us.ibm.com>
2018-05-18 14:27:25 +0000

    Automated CAMHub::publish on 2018-05-18 14:27:25 UTC

--- chef/cookbooks/wmq/attributes/internal.rb
+++ chef/cookbooks/wmq/attributes/internal.rb
@@ -121,7 +121,7 @@ when '8.0'
                     'sha256' =>  '6d1db6949a2a97606eb62cc7f43977bbdf61bdd229f1a1085ad05b6fb800f176' } }
       force_default['wmq']['fixpack_names'] = {
         'fixpack' => { 'filename' => "8.0.0-WS-MQ-LinuxX64-FP000#{node['wmq']['fixpack']}.tar.gz" } }
-      force_default['wmq']['prereqs'] = %w(ksh binutils compat-libstdc++-33 compat-libstdc++-33.i686 pam.i686 gcc gcc-c++ gcc glibc libgcc openssl gtk2 libstdc++.i686 libstdc++ redhat-lsb-core)
+      force_default['wmq']['prereqs'] = %w(ksh binutils compat-libstdc++-33 compat-libstdc++-33.i686 pam pam.i686 gcc gcc-c++ gcc glibc libgcc openssl gtk2 libstdc++.i686 libstdc++ redhat-lsb-core)
     when 'powerpc'
       force_default['wmq']['archive_names'] = {
         'base' => { 'filename' => 'WS_MQ_FOR_AIX_V8.0_EIMAGE.tar.tgz',
@@ -172,7 +172,7 @@ when '9.0'
                     'sha256' =>  'd16efd8113bede1439c1be4865befe7f3193648b70f08646a0fa0ad1a42a996a' } }
       force_default['wmq']['fixpack_names'] = {
         'fixpack' => { 'filename' => "9.0.0-IBM-MQ-LinuxX64-FP000#{node['wmq']['fixpack']}.tar.gz" } }
-      force_default['wmq']['prereqs'] = %w(ksh binutils compat-libstdc++-33 compat-libstdc++-33.i686 pam.i686 gcc gcc-c++ gcc glibc libgcc openssl gtk2 libstdc++.i686 libstdc++ redhat-lsb-core)
+      force_default['wmq']['prereqs'] = %w(ksh binutils compat-libstdc++-33 compat-libstdc++-33.i686 pam pam.i686 gcc gcc-c++ gcc glibc libgcc openssl gtk2 libstdc++.i686 libstdc++ redhat-lsb-core)
     when 'powerpc'
       force_default['wmq']['archive_names'] = {
         'base' => { 'filename' => 'WS_MQ_FOR_AIX_V9.0.0_EIMAGE.tar.tgz',
--- chef/cookbooks/wmq/metadata.rb
+++ chef/cookbooks/wmq/metadata.rb
@@ -162,7 +162,7 @@ default['ibm_internal']['vault']['item']       - Vault Item to reference, will b
 ```
 EOH
 
-version '1.0.5'
+version '1.0.6'
 attribute 'wmq/advanced',
           :choice => ['true', 'false'],
           :default => 'false',
